cc1ecac23776d274fed50c0f916856d31070d5c0
cenmiao <miao_cen@163.com>
2024-02-08 23:22:09 +0800

    initial submit

--- src/main/java/com/kuang/handler/MyMetaObjectHandler.java
+++ src/main/java/com/kuang/handler/MyMetaObjectHandler.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Slf4j
 @Component // 一定不要忘记把处理器加到IOC容器中！
 public class MyMetaObjectHandler implements MetaObjectHandler {
-    // 插入时的填充策略
+    // 插入时的填充策略sdfsdfsds
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill.....");
