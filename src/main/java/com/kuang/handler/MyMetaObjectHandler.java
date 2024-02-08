95f26c544dc0329f15c5cee00c9f0b9ae5e26b40
cenmiao <miao_cen@163.com>
2024-02-08 23:24:17 +0800

    test01

--- src/main/java/com/kuang/handler/MyMetaObjectHandler.java
+++ src/main/java/com/kuang/handler/MyMetaObjectHandler.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Slf4j
-@Component // 一定不要忘记把处理器加到IOC容器中！
+@Component // 一定不要忘记把处理器加到IOC容器中！sdfgsdfg
 public class MyMetaObjectHandler implements MetaObjectHandler {
-    // 插入时的填充策略sdfsdfsds
+    // 插入时的填充策略sdfsdfsdsfdsdf
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill.....");
